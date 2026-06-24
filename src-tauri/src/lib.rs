2e3cf939a14bdeb962497e363f9309eedb97b3d0
Etheriouse <Etheriouse.7@gmail.com>
2026-06-25 01:34:36 +0200

    mock tauri for front

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,15 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
+
+// Like @Override its a identifier for function that can call by front
 #[tauri::command]
+// my function 
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+
+// Don't touch, tauri library
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
